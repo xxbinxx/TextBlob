23a2dd13b14693be55e46529ffc852708327f64b
Bineet Kumar Gaur - Pythonist (---===e  Max) <bineetkumar@live.in>
2021-12-09 23:59:13 +0530

    Fixing 404 translate url

--- textblob/translate.py
+++ textblob/translate.py
@@ -30,7 +30,8 @@ class Translator(object):
         u'es'
     """
 
-    url = "http://translate.google.com/translate_a/t?client=webapp&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&ssel=0&tsel=0&kc=1"
+#     url = "http://translate.google.com/translate_a/t?client=webapp&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&ssel=0&tsel=0&kc=1"
+    url = "http://translate.google.com/translate_a/t?client=te&format=html&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&ssel=0&tsel=0&kc=1"
 
     headers = {
         'Accept': '*/*',
